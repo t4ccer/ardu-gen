c720f53c28d3f40b592c686b02dfc30e7d12b612
t4ccer <64430288+t4ccer@users.noreply.github.com>
2020-08-07 17:35:27 +0200

    Add pubsubclient funcs

--- src/ArduGen/ESP32/PubSubClient.hs
+++ src/ArduGen/ESP32/PubSubClient.hs
@@ -1,7 +1,26 @@
 module ArduGen.ESP32.PubSubClient where
 
 import ArduGen.Base
+import ArduGen.Byte
 
 data PubSubClient
 instance ClassClass PubSubClient where
   className _ = "PubSubClient"
+
+mqttSetServer :: LVal (Class PubSubClient) -> Fun(String -> Int -> IO ())
+mqttSetServer client = declareFunExtern (unVal client ++ ".setServer")
+
+mqttConnected :: LVal (Class PubSubClient) -> Fun (IO Int)
+mqttConnected client = declareFunExtern (unVal client ++ ".connected")
+
+mqttSetCallback :: LVal (Class PubSubClient) ->  Fun (Fun (Ptr Char -> Ptr Byte -> Int -> IO ()) -> IO ())
+mqttSetCallback client = declareFunExtern (unVal client ++  ".setCallback")
+
+mqttConnect :: LVal (Class PubSubClient) ->  Fun (String -> IO ())
+mqttConnect client = declareFunExtern (unVal client ++ ".connect")
+
+mqttLoop :: LVal (Class PubSubClient) ->  Fun (IO ())
+mqttLoop client = declareFunExtern (unVal client ++  ".loop")
+
+mqttPublish :: LVal (Class PubSubClient) ->  Fun(String -> Ptr Char -> IO ())
+mqttPublish client = declareFunExtern (unVal client ++ ".publish")
