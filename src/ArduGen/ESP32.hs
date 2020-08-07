643f28f078ceb12bcb66e0f3d75defe440069f64
t4ccer <64430288+t4ccer@users.noreply.github.com>
2020-08-07 15:48:42 +0200

    Add esp32 restart func

--- src/ArduGen/ESP32.hs
+++ src/ArduGen/ESP32.hs
@@ -1,7 +1,9 @@
 module ArduGen.ESP32
-    ( 
+    (
+    -- Base
+      restartESP
     -- PWM
-      ledcAttachPin
+    , ledcAttachPin
     , ledcSetup
     , ledcWrite
     -- PubSubClient
@@ -10,3 +12,7 @@ module ArduGen.ESP32
 
 import ArduGen.ESP32.PWM
 import ArduGen.ESP32.PubSubClient
+import ArduGen.Base (declareFunExtern, Fun)
+
+restartESP :: Fun (IO ())
+restartESP = declareFunExtern "ESP.restart"
